b1190482b2b7b31490edffecafc0d7d855a06cfc
Michał Januszko <michaljanuszko@gmail.com>
2021-04-17 17:18:36 +0200

    Added Events list

---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import EventListItem from "./EventListItem";
+
+const EventList = () => {
+    return (
+        <>
+            <h1>List</h1>
+            <EventListItem />
+            <EventListItem />
+            <EventListItem />
+            <EventListItem />
+            <EventListItem />
+        </>
+    );
+};
+
+export default EventList;
